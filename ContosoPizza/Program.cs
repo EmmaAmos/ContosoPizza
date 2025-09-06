faa46af236adca0b9722955b651cfd1ff45f56ea
EmmaAmos <elovestodraw@gmail.com>
2025-09-05 22:44:17 -0400

    trying to connect

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,19 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// In development, enable Swagger UI and Swagger JSON.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+// These are for routing and security, and must come AFTER the Swagger middleware.
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
